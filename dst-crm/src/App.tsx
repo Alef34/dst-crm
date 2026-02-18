8bebeb232a85c03e7c3f9671dadac01d324ac0f9
Ninka Fabianova <ninafabianova18@gmail.comm>
2026-02-18 15:00:15 +0100

    Update dependencies and App component

--- dst-crm/src/App.tsx
+++ dst-crm/src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import { ProtectedRoute } from './components/ProtectedRoute'
 import { Login } from './components/Login'
-import { AccessRequest } from './components/AccessRequest'
+//import { AccessRequest } from './components/AccessRequest'
 import { Dashboard } from './components/Dashboard'
 import { Unauthorized } from './components/Unauthorized'
 
@@ -15,7 +15,7 @@ function App() {
       <AuthProvider>
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/access-request" element={<AccessRequest />} />
+          {/*<Route path="/access-request" element={<AccessRequest />} />*/}
           <Route path="/unauthorized" element={<Unauthorized />} />
           
         
